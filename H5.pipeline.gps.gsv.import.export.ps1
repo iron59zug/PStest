092405834691abc8b1529dd48f7120defb47358b
classified <65862803+iron59zug@users.noreply.github.com>
2020-06-24 11:39:52 +0200

    Update H5.pipeline.gps.gsv.import.export.ps1

--- H5.pipeline.gps.gsv.import.export.ps1
+++ H5.pipeline.gps.gsv.import.export.ps1
@@ -19,17 +19,14 @@ spsv abc
 # maar wel door een puntkomma gescheiden worden. Hoe zou je dit doen met Export-CSV? 
 epcsv -d ";" #-Delimiter
 
-# 5. In Windows wordt er gebruik gemaakt van landinstellingen om onder andere het
-# lijstscheidingsteken te bepalen. Hoe kan je aan Export-CSV duidelijk maken dat moet
-# gebruik gemaakt worden van het scheidingsteken ingesteld door het systeem in plaats van een komma?
+# 5. Windows heeft landinstellingen voor o.a. het lijstscheidingsteken. 
+# maak aan Export-CSV duidelijk het scheidingsteken vh systeem te gebruiken ipv komma:
 epcsv -u #-UseCulture
 
-# 6. Export-CliXML en Export-CSV kunnen gegevens wijzigen op een computer. Hoe kan 
-# je voorkomen dat deze programma’s een bestaand bestand overschrijven?
+# 6. Export-CliXML en Export-CSV wijzigen gegevens op computer. Hoe voorkomen een bestaand bestand overschrijven?
 epcsv -append # add output to the end of the file
 epcsv -NoClobber # no overwrite
 # -force will overwrite read-only file
 
-# 7. Als je gebruik maakt van Export-CSV dan begint het resultaat met als eerste lijn een
-# commentaarlijn (start met #). Hoe kan je deze lijn weglaten bij opmaken van het bestand?
+# 7. resultaat van Export-CSV begint een commentaarlijn. Hoe weglaten?
 epcsv -NoTypeInformation
