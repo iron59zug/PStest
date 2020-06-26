bfaaf192497cc5abe09e4b1b38cb437010f765e7
classified <65862803+iron59zug@users.noreply.github.com>
2020-06-26 22:25:49 +0200

    Update and rename H8.pipeline.ADuser.ps5 to H8.pipeline.AD.ps1

--- H8.pipeline.AD.ps1
+++ H8.pipeline.AD.ps1
@@ -3,3 +3,4 @@
 ipcsv c:\path\to\file.csv | Select-Object *, @{l='label';e={$_.expression}} | new-ADuser -path 'ou=MyOU,DC=me,DC=local'
 # => create new column/property 'label', w/ value from expression column
 
+
