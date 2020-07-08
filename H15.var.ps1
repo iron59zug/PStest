27e387c90aa16eedbb3dea3c26f39d5b501322db
classified <65862803+iron59zug@users.noreply.github.com>
2020-07-08 23:06:29 +0200

    Create H15.var.ps1

---
+++ H15.var.ps1
@@ -0,0 +1,15 @@
+$var1 = 'value'
+$v_2 = read-host "user input: "
+$v3 = $v3+1
+#<#
+[bool]$Boolean = {True|False}
+[byte]$Byte = {0-255} = Unsigned 8-bit
+[char]$Char = one_character
+[int]$Integer = a_numeric_data_type; ==[int32] = 32-bit signed integer = default
+[int64]
+[float]
+[decimal]$Decimal = a_128-bit_data_type; more precise than floating point types
+[string]$String = "some char's"
+[date]$date = 
+[xml]$xml_doc =
+##>
