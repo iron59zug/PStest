bf8d88c80276829291ed57faebafefa6c9a326f2
classified <65862803+iron59zug@users.noreply.github.com>
2020-07-12 01:33:20 +0200

    Update H19.tips.tricks.ps1

--- H19.tips.tricks.ps1
+++ H19.tips.tricks.ps1
@@ -9,3 +9,7 @@ $DoW = @{l='DoW';e={$_.TimeGenerated.DayOfWeek}}
 
 # idea 2: pipeline orders
 Get-EventLog | sort | ft -gr
+
+# idea 3: H8 eg.3
+get-adcomputer –filter * -searchbase “ou=domain controllers,dc=opleidingen,dc=intra” | 
+select-object @{l=’computername’;e={$_.name}} | get-hotfix
