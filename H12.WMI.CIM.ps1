6d893bdd35d29311d1d0bfdfefdd47c2f6066437
classified <65862803+iron59zug@users.noreply.github.com>
2020-07-12 03:30:14 +0200

    Update H12.WMI.CIM.ps1

--- H12.WMI.CIM.ps1
+++ H12.WMI.CIM.ps1
@@ -1,9 +1,16 @@
 # gwmi: [–namespace root\cimv2] = default path, [-class/-property] = position 1
 Get-wmiobject –namespace root\cimv2 –class win32_desktop # == gwmi win32_desktop 
+# WMI explorer, FOSS+portable: wmie2, Sapien: CIM Explorer
 
-# 1. which class can be used to get the ip of an NIC? can it handle DHCP refresh?
+#eg.1 which class for finding the ip of an NIC, can it handle DHCP refresh?
 class: \\IP32015IKB\ROOT\CIMV2:Win32_NetworkAdapterConfiguration
 method: RenewDHCPLease 
 
-# 2. get services & show name, status, startup mod, user; tip: gsv cannot get startup mod
+#eg.2 get services & show name, status, startup mod, user; tip: gsv cannot get startup mod
 gwmi win32_service | select name,status,startmode,startname | ft
+
+#eg.3 get installed hotfix w/ gwmi, tip: ‘quickfix engineering’
+gwmi Win32_QuickFixEngineering
+
+#eg.4 list PCname, OS build n°, OS description(caption), BIOS sn:
+gwmi Win32_...
